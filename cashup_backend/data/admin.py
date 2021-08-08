05fec78ac77eb10120c3cf148b98a78a2e03d26f
MinJunsu <sunsuking@gmail.com>
2021-08-08 16:42:10 +0900

    새로운 방식의 signal을 위해 데이터 수집 업데이트

--- cashup_backend/data/admin.py
+++ cashup_backend/data/admin.py
@@ -5,10 +5,10 @@ from .models import HourData, MinuteData
 # Register your models here.
 @admin.register(HourData)
 class HourDataAdmin(admin.ModelAdmin):
-    list_display = ['datetime', 'max_price', 'min_price', 'continue_up_down', 'signal', 'volume_rate', 'work_two_1_5', 'work_one_1_0', 'work_one_1_5', ]
+    list_display = ['datetime', 'open_price', 'min_price', 'max_price', 'close_price', 'up_down', 'continue_up_down', 'volume_rate', 'signal', 'work_one_1_0']
     search_fields = ['signal']
 
 
 @admin.register(MinuteData)
 class MinuteDataAdmin(admin.ModelAdmin):
-    list_display = ['datetime', 'open_price', 'min_price', 'max_price', 'close_price']
+    list_display = ['datetime', 'open_price', 'min_price', 'max_price', 'close_price', 'up_down', 'continue_up_down', 'volume_rate', 'signal', 'work_one_1_0']
