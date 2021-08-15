238d92ac9c1c2fd725d3e556601dc5a638a12ce0
junsu <sunsuking@gmail.com>
2021-08-16 01:43:24 +0900

    수정값 변경 20개

--- cashup_backend/collect_realtime_data_5m.py
+++ cashup_backend/collect_realtime_data_5m.py
@@ -298,7 +298,7 @@ for count in range(len(req)):
         now_work_1_5_2 = "w" + now_work_1_5_2
     
 
-    if count > 890:
+    if count >980:
         default = {
             'datetime': datetime,
             'open_price': open_price,
