d82f0e8518a31ceaf5170c26b28876f4da713f1a
junsu <sunsuking@gmail.com>
2021-08-21 13:57:59 +0900

    데이터 수집 로직 최종 수정 더 이상 수정 없을 예정

--- cashup_backend/collect_realtime_data_5m.py
+++ cashup_backend/collect_realtime_data_5m.py
@@ -449,7 +449,7 @@ for num_count in range(len(req)):
                 }
                 DownFlow.objects.update_or_create(datetime=flow_element.datetime, defaults=default)
 
-    if num_count > 0:
+    if num_count > 980:
         default = {
             'datetime': datetime,
             'open_price': open_price,
